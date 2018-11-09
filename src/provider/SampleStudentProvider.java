604209de538ebfd0d7674894903be49ec6699699
Dmytro Lutsiuk <34756558+LutsiukD@users.noreply.github.com>
2018-11-09 13:49:31 +0200

    Update SampleStudentProvider.java

--- src/provider/SampleStudentProvider.java
+++ src/provider/SampleStudentProvider.java
@@ -15,17 +15,20 @@ public class SampleStudentProvider {
         Student s1 = new Student("Andrew Kostenko");
         Student s2 = new Student("Julia Veselkina");
         Student s3 = new Student("Maria Perechrest");
+        Student s4 = new Student("Dmytro Lutsiuk");
+		Student s5 = new Student("Ivan Petriv ");
 
-        Knowledge lowKnowledge = new Knowledge(1);
-        Knowledge higherThanMediumKnowledge = new Knowledge(3);
-
-        s1.setKnowledge(lowKnowledge);
-        s2.setKnowledge(higherThanMediumKnowledge);
-        s3.setKnowledge(higherThanMediumKnowledge);
+        s1.setKnowledge(new Knowledge(1));
+        s2.setKnowledge(new Knowledge(3));
+        s3.setKnowledge(new Knowledge(3));
+        s4.setKnowledge(new Knowledge(4));
+		s5.setKnowledge(new Knowledge(5));
 
         students.add(s1);
         students.add(s2);
         students.add(s3);
+        students.add(s4);
+		students.add(s5);
 
         return students;
     }

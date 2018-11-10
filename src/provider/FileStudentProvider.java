8a0215a1f8bc242e255b9507975547a44497c9be
Dmytro Lutsiuk <34756558+LutsiukD@users.noreply.github.com>
2018-11-10 23:38:46 +0200

    Update FileStudentProvider.java

--- src/provider/FileStudentProvider.java
+++ src/provider/FileStudentProvider.java
@@ -4,8 +4,9 @@ import person.Student;
 
 import java.util.List;
 
-public class FileStudentProvider {
-
+public class FileStudentProvider extends StudentProvider {
+    
+    @Override
     public List<Student> getAllStudents() {
         return null;
     }

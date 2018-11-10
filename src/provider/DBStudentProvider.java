c19985adb2d4db1d61ec0cd009ce0452f3d84986
Dmytro Lutsiuk <34756558+LutsiukD@users.noreply.github.com>
2018-11-10 23:26:37 +0200

    Update DBStudentProvider.java

--- src/provider/DBStudentProvider.java
+++ src/provider/DBStudentProvider.java
@@ -4,8 +4,9 @@ import person.Student;
 
 import java.util.List;
 
-public class DBStudentProvider {
-
+public class DBStudentProvider extends StudentProvider {
+    
+    @Override
     public List<Student> getAllStudents() {
         return null;
     }
